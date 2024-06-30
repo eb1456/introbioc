7cbbe5168af20ecbbf27a73f2c4870751d022427
Ezra Baraban <ezra.baraban@gmail.com>
2024-06-30 19:51:07 -0400

    1st commit

---
+++ course1.R
@@ -0,0 +1,59 @@
+# https://genomicsclass.github.io/book/ chapters 11, 12, 13
+
+BiocManager::version()
+
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+BiocManager::install(c("genefilter","geneplotter"))
+
+library(BSgenome.Hsapiens.UCSC.hg19)
+library(genefilter)
+library(geneplotter)
+
+# get help through the documentation
+help.start()
+?mean
+help(mean)
+help(package="genefilter")
+
+# inspect objects, classes and methods
+library(Biobase)    # load one of the core Bioconductor packages
+?ExpressionSet
+?"ExpressionSet-class"
+methods(class = ExpressionSet)
+
+# inspect the source code for functions and methods
+read.csv
+plotMA
+showMethods("plotMA")
+getMethod("plotMA","data.frame")
+
+# vignettes teach you how to use various functions in a package
+vignette(package="Biobase")
+vignette("ExpressionSetIntroduction")
+browseVignettes(package="Biobase")
+
+# report key details about your R session
+sessionInfo()
+
+BiocManager::version()
+
+###
+BiocManager::install(c("genefu",
+                       "COPDSexualDimorphism",
+                       "gwascat",
+                       "hgu133a.db",
+                       "genomicsclass/tissuesGeneExpression"))
+
+#explore mammaprint genes
+library(genefu)
+data(sig.gene70)
+dim(sig.gene70)
+head(sig.gene70)[,1:6]
+
+#explore COPD
+library(COPDSexualDimorphism.data)
+data(lgrc.expr.meta)
+
+library(gwascat)
+data(ebicat_2020_04_30)
+ebicat_2020_04_30
